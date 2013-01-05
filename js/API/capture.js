314d1c682c459b7eba614591f6dbf37503f65eac
Alejandro <alejmlara@me.com>
2013-01-04 21:06:06 -0600

    Código de captura añadido

--- js/API/capture.js
+++ js/API/capture.js
@@ -5,15 +5,32 @@ function pgAlert(msj,tit){ navigator.notification.alert(msj, null, tit, 'Aceptar
 function deviceReady(){
 	//Obteniendo taps de los botones
 	$('#capturar .rounded .arrow').tap(function(){
+	
 		switch($(this).index()){
+	
 			case 0: //Grabar Audio
-				
+				navigator.device.capture.captureAudio(function(mF){
+					for(i=0; i<mF.length; i++){
+						pgAlert(mf[i].fullpath, "Audio");
+					}
+				}, errorCaptura, { limit: 2 });
 				break;
+
 			case 1: //Grabar Video
-				
+				navigator.device.capture.captureVideo(function(mF){
+					for(i=0; i<mF.length; i++){
+						pgAlert(mf[i].fullpath, "Vídeo");
+					}
+				}, errorCaptura, { limit: 2 });
 				break;
+	
 			case 2: //Capturar Imagen
-				
+			navigator.device.capture.captureImage(function(mF){
+					for(i=0; i<mF.length; i++){
+						pgAlert(mf[i].fullpath, "Imagen");
+					}
+				}, errorCaptura, { limit: 2 });
+				break;	
 		}
 	});
 }
